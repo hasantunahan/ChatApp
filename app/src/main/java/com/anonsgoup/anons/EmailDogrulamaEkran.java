8789e02993a56eb12a639affdbcbd96601b953b7
Hasan Tunahan <name_tunahan@hotmail.com>
2019-02-22 11:16:24 +0300

    iiii

--- app/src/main/java/com/anonsgoup/anons/EmailDogrulamaEkran.java
+++ app/src/main/java/com/anonsgoup/anons/EmailDogrulamaEkran.java
@@ -3,7 +3,7 @@ package com.anonsgoup.anons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class email_dogrulama extends AppCompatActivity {
+public class EmailDogrulamaEkran extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
