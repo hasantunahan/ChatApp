e7ceffc407d1c774dbe526fd8edc5e5bd9855ede
Hasan Tunahan <name_tunahan@hotmail.com>
2019-03-13 23:17:32 +0300

    Merge remote-tracking branch 'remotes/origin/master' into hasan
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/res/layout/activity_profil.xml

--- app/src/main/java/com/anonsgroup/anons/ChatEkran.java
+++ app/src/main/java/com/anonsgroup/anons/ChatEkran.java
@@ -1,13 +1,14 @@
 package com.anonsgroup.anons;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +27,7 @@ public class ChatEkran extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FloatingActionButton mesajAt;
 
     private OnFragmentInteractionListener mListener;
 
@@ -60,11 +62,22 @@ public class ChatEkran extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_ekran, container, false);
+        final View view = inflater.inflate(R.layout.fragment_chat_ekran, container, false);
+        mesajAt=view.findViewById(R.id.mesajAtButton);
+        mesajAt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(view.getContext(),ArkadasListesiEkran.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
