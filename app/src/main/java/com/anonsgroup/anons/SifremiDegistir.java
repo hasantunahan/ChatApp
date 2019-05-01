db820cc15afedc11beb12a7770d1cf2fc8a77a82
serocan38 <s.o.gs@hotmail.com>
2019-05-01 20:55:42 +0300

    Merge remote-tracking branch 'remotes/origin/tarik' into sero
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/anonsgroup/anons/AnaEkran.java
    #	app/src/main/java/com/anonsgroup/anons/ChatEkran.java

--- app/src/main/java/com/anonsgroup/anons/SifremiDegistir.java
+++ app/src/main/java/com/anonsgroup/anons/SifremiDegistir.java
@@ -2,9 +2,11 @@ package com.anonsgroup.anons;
 
 import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
@@ -23,14 +25,27 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.regex.Pattern;
+
 public class SifremiDegistir extends AppCompatActivity {
         EditText yeniET1,yeniET2;
         Button sifreDegisButton;
-        DatabaseReference ref;
-        private String str;
-    private FirebaseAuth auth;
-    private FirebaseUser firebaseUser;
-    private FirebaseAuth.AuthStateListener authListener;
+        private FirebaseUser firebaseUser;
+        Editable sifre;
+        TextInputLayout passwordWrapper;
+        boolean kontrol = true;
+
+        private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    //"(?=.*[0-9])" +         //at least 1 digit
+                    //"(?=.*[a-z])" +         //at least 1 lower case letter
+                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                    "(?=.*[0-9])" +
+                    "(?=.*[a-zA-Z])" +      //any letter
+                    "(?=.*['*!@#$%^&+=.,_-])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{6,}" +               //at least 6 characters
+                    "$");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,33 +54,28 @@ public class SifremiDegistir extends AppCompatActivity {
         yeniET1 = findViewById(R.id.yeniET1);
         yeniET2 = findViewById(R.id.yeniET2);
         sifreDegisButton = findViewById(R.id.sifreDegisButton);
-
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         sifreDegisButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str = "Lütfen yeni şifreyi giriniz.";
-                changeEmailOrPasswordFunc(str,false);
+                sifre= yeniET1.getText();
+                firebaseUser.updatePassword(String.valueOf(sifre));
             }
         });
-
-
-        }
-    private void changeEmailOrPasswordFunc(String title, final boolean option) {
-
-        if(!option){  // password type
-            yeniET1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         }
 
+
+        private boolean kontroller() {
+            String passwordInput = yeniET1.getText().toString().trim();
+            if (passwordInput.isEmpty()) {
+                passwordWrapper.setError(getResources().getString(R.string.empty_warning));
+                kontrol = false;
+            } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+                passwordWrapper.setError(getResources().getString(R.string.password_warning));
+                kontrol = false;
+            } else {
+                passwordWrapper.setError(null);
             }
-
-            private void changePassword() {
-
-                firebaseUser.updatePassword(yeniET1.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful())
-                                    Toast.makeText(getApplicationContext(), "Şifre değiştirildi.", Toast.LENGTH_LONG).show();
-                            }});
-}
+            return kontrol;
+        }
 }
